19419ecd83e5abc23dcdd90685cb8319495f249b
Andy Smart <64802974+absm-art@users.noreply.github.com>
2025-04-19 18:18:19 -0500

    fixes and some adjustments

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,7 +2,7 @@ var store = [{
         "title": "Let's Test Some Things",
         "excerpt":"Hmmmm Okay, I’m testing out the posting system to see if this makes sense. Various formatting tricks will probably break this functionality, but for now we’re ballin - This site was made using Ruby and Jekyll, and is deployed using GitHub Pages via a domain name acquired at PorkBun. The...","categories": ["journal"],
         "tags": ["personal"],
-        "url": "/journal/absm.art/",
+        "url": "/journal/Checking-Things/",
         "teaser": "/assets/images/500x300.PNG"
       },{
         "title": "About This Site",
@@ -14,13 +14,13 @@ var store = [{
         "title": "Something meaningful",
         "excerpt":"Well, it’s certainly a strange time in history to be alive. I suppose, aside from the ‘about me’ page and the inaugural ‘what the hell is a website’ post, this is the first time I’m writing something that will be published under my own name, as a part of my...","categories": ["journal"],
         "tags": ["personal"],
-        "url": "/journal/absm.art/",
+        "url": "/journal/Something-Meaningful/",
         "teaser": "/assets/images/500x300.PNG"
       },{
         "title": "Setting Up a Workflow",
         "excerpt":"This is a test to see if the Obsidian post creation workflow is gonna get the results I want– I can create markdown files, sure, but are they weird somehow? The process for making them requires Obsidian to install directory files in the root of the website assets, but GitHub...","categories": ["website"],
         "tags": ["personal","work","website","Obsidian","GitHub"],
-        "url": "/website/absm.art/",
+        "url": "/website/Setting-Up-A-Workflow/",
         "teaser": "/assets/images/500x300.PNG"
       },{
     "title": "Archive: Categories",
